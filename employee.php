d78ca6af8c4a245f7e38177ecbbb9b7cd8de60ca
Mathew-Varghese28 <mca526@rajagiri.edu>
2021-03-10 15:22:35 +0530

    Responsive

---
+++ employee.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table class="table">
+        <tr>
+            <td>
+                NAME
+            </td>
+            <td>
+                <input class="form-control" type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                name
+            </td>
+            <td>
+                <input class="form-control" type="text">
+            </td>
+            
+        </tr>
+        <tr>
+            <td>
+                Designation
+            </td>
+            <td><input class="form-control" type="text"></td>
+         
+        </tr>
+        <tr>
+            <td>salary</td>
+            <td><input class="form-control" type="number"></td>
+          
+        </tr>
+        <tr>
+            <td>Company Name</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>Place</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>Date of Birth</td>
+            <td><input class="form-control" type="date"></td>
+          
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td><input class="form-control" type="email"></td>
+         
+        </tr>
+        <tr>
+            <td>Username</td>
+            <td><input class="form-control" type="text"></td>
+         
+        </tr>
+        <tr>
+            <td>Passward</td>
+            <td><input class="form-control" type="password"></td>
+          
+        </tr>
+        <tr>
+            <td>Conform Passward</td>
+            <td><input class="form-control" type="password"></td>
+           
+        </tr>
+        <tr>
+            <td></td>
+            <td><button class="btn btn-danger">
+                Register
+            </button></td>
+        </tr>
+    </table>
+</body>
+</html>
